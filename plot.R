d67aa66490ca7d1c045bda8af06c4d1a5a8622de
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-06-16 15:52:48 +0200

    har: generalize many things to n dimensions

--- plot.R
+++ plot.R
@@ -23,9 +23,6 @@ plotWeights <- function(W) {
 library('rgl')
 library('far') # for orthonormalization
 
-basis <- matrix(c(1, 0, -1, 0, 1, -1), nrow=3)
-on <- orthonormalization(basis, basis=F, norm=T)
-
 drawVector <- function(v, color="black", alpha=0.6) {
 	lines3d(c(0, v[1]), c(0, v[2]), c(0, v[3]), color=color, alpha=alpha)
 }
@@ -53,14 +50,22 @@ testTransformation <- function() {
 
 # HAR
 
+# generate a random direction in n-dimensional space
+# FIXME: implement for n != 2
+randDir <- function(n) {
+	stopifnot(n == 2)
+	d <- runif(1, 0, 2*pi)[1] # random direction
+	c(cos(d), sin(d)) # transform to x/y
+}
+
 # x0: starting point; niter: number of iterations; bound: bounding box function; hit: hit determination function.
 har <- function(x0, niter, bound, hit) {
 	misses <- 0
 	x <- as.list(rep(0, niter))
+	n <- dim(x0)
 	x[[1]] <- x0
 	for (i in 2:niter) {
-		d <- runif(1, 0, 2*pi)[1] # random direction
-		d <- c(cos(d), sin(d)) # transform to x/y
+		d <- randDir(n) # random direction
 		bounds <- bound(x[[i-1]], d)
 		wasHit <- FALSE
 		while (!wasHit) {
@@ -85,25 +90,17 @@ har <- function(x0, niter, bound, hit) {
 	list(x, misses)
 }
 
-# a bounding box of sqrt(2/3) around origin
-boundBox <- function(x, d) {
-	lb <- c(-sqrt(2/3), -sqrt(2/3)) #FIXME: bounds correct for all n?
-	ub <- c(sqrt(2/3), sqrt(2/3))
-	c(
-		max(sapply(1:length(x), function(i) { min((lb[i] - x[i]) / d[i], (ub[i] - x[i]) / d[i]) })),
-		min(sapply(1:length(x), function(i) { max((lb[i] - x[i]) / d[i], (ub[i] - x[i]) / d[i]) }))
-	)
-}
-
 # hit calculation for (n-1)-dimensional simplex
 # basis: orthonormal basis for the (n-1)-dimensional simplex in n-dimensional space
 createHitSimplex <- function(basis) {
+	n <- dim(basis)[1]
+	a1 <- diag(rep(-1, n)) # -1*w[i] <= 0
+	a2 <- 1 + c1 # (\sum w[j]) - w[i] <= 1
+	a <- cbind(rbind(a1, a2), c(rep(0, n), rep(1, n)))
 	function(x) {
 		x <- basis %*% x # change of basis
-		w <- 1/3 + x # translation (FIXME: correct for all n?)
-		allBounds <- min(sapply(w, function(wi) { wi >= 0 }))
-		sumBounds <- min(c(w[1] + w[2] <= 1, w[1] + w[3] <= 1, w[2] + w[3] <= 1)) # FIXME: generalize
-		allBounds && sumBounds #&& w[1] <= 0.5 && w[2]/w[3] >= 0.3 && w[2]/w[3] <= 1.1
+		w <- rbind(1/n + x, -1) # translation
+		min(a %*% w <= 0)
 	}
 }
 
@@ -116,7 +113,7 @@ inverse <- function(mat) {
 # create a bounding box given a (n-1) basis and extreme points in R^n
 createBoundBox <- function(basis, extreme) {
 	n <- dim(basis)[2]
-	extreme <- t(basis) %*% (extreme - 1/3) # extreme points
+	extreme <- t(basis) %*% (extreme - 1/n) # extreme points
 	lb <- apply(extreme, 1, min)
 	ub <- apply(extreme, 1, max)
 	function(x, d) {
@@ -127,6 +124,8 @@ createBoundBox <- function(basis, extreme) {
 	}
 }
 
+basis <- matrix(c(1, 0, -1, 0, 1, -1), nrow=3)
+on <- orthonormalization(basis, basis=F, norm=T)
 hit <- createHitSimplex(on)
 bound <- createBoundBox(on, diag(3))
 samples <- har(c(0,0), 1000, bound, hit)
