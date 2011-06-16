7497e87267abca9bc00c123e8f832f57af82f0f4
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-06-16 14:25:02 +0200

    har: correct bounding box, cleanup

--- plot.R
+++ plot.R
@@ -23,31 +23,33 @@ plotWeights <- function(W) {
 library('rgl')
 library('far') # for orthonormalization
 
+basis <- matrix(c(1, 0, -1, 0, 1, -1), nrow=3)
+on <- orthonormalization(basis, basis=F, norm=T)
+
 drawVector <- function(v, color="black", alpha=0.6) {
 	lines3d(c(0, v[1]), c(0, v[2]), c(0, v[3]), color=color, alpha=alpha)
 }
 
-# draw the polytope translated to origin
-plot3d(c(0), c(0), c(0), xlim=c(-1/3, 2/3), ylim=c(-1/3, 2/3), zlim=c(-1/3, 2/3))
-triangles3d(c(2/3,-1/3,-1/3), c(-1/3,2/3,-1/3), c(-1/3,-1/3,2/3), alpha=0.3)
+testTransformation <- function() {
+	# draw the polytope translated to origin
+	plot3d(c(0), c(0), c(0), xlim=c(-1/3, 2/3), ylim=c(-1/3, 2/3), zlim=c(-1/3, 2/3))
+	triangles3d(c(2/3,-1/3,-1/3), c(-1/3,2/3,-1/3), c(-1/3,-1/3,2/3), alpha=0.3)
 
-# draw the basis
-basis <- matrix(c(1, 0, -1, 0, 1, -1), nrow=3)
-drawVector((1/3)*basis[,1])
-drawVector((1/3)*basis[,2])
+	# draw the basis
+	drawVector((1/3)*basis[,1])
+	drawVector((1/3)*basis[,2])
 
-# draw the orthonormal basis
-on <- orthonormalization(basis, basis=F, norm=T)
-drawVector((1/3)*on[,1], color="red", alpha=1)
-drawVector((1/3)*on[,2], color="red", alpha=1)
+	# draw the orthonormal basis
+	drawVector((1/3)*on[,1], color="red", alpha=1)
+	drawVector((1/3)*on[,2], color="red", alpha=1)
 
 
-# sample random directions
-dir <- runif(100, 0, 2*pi)
-coord <- matrix(c(cos(dir), sin(dir)), ncol=2)
-samples <- t(sapply(1:dim(coord)[1], function(i) { on %*% coord[i,] }))
-points3d(samples)
-
+	# sample random directions
+	dir <- runif(100, 0, 2*pi)
+	coord <- matrix(c(cos(dir), sin(dir)), ncol=2)
+	samples <- t(sapply(1:dim(coord)[1], function(i) { on %*% coord[i,] }))
+	points3d(samples)
+}
 
 # HAR
 
@@ -55,58 +57,17 @@ points3d(samples)
 har <- function(x0, niter, bound, hit) {
 	x <- as.list(rep(0, niter))
 	x[[1]] <- x0
-	wasHit <- TRUE
-	d <- c(0, 0)
-	bounds <- c(0, 0)
 	for (i in 2:niter) {
-		print(wasHit)
-		if (wasHit) {
-			d <- runif(1, 0, 2*pi)[1] # random direction
-			d <- c(cos(d), sin(d))
-			bounds <- bound(x[[i-1]], d)
-		}
-		print(d)
-		print(bounds)
-		l <- runif(1, bounds[1], bounds[2])[1] # random distance
-		xN <- x[[i]] + l * d
-		print(xN)
-		if (hit(xN)) {
-			x[[i]] <- xN
-			wasHit <- TRUE
-		} else {
-			x[[i]] <- x[[i-1]]
-			wasHit <- FALSE
-			# update bounds
-			if (l > 0) {
-				bounds[2] <- l
-			}
-			if (l < 0) 
-				bounds[1] <- l
-			}
-		}
-	}
-	x
-}
-
-# x0: starting point; niter: number of iterations; bound: bounding box function; hit: hit determination function.
-har <- function(x0, niter, bound, hit) {
-	x <- as.list(rep(0, niter))
-	x[[1]] <- x0
-	for (i in 2:niter) {
-		print(paste(c("Iteration", i)))
 		d <- runif(1, 0, 2*pi)[1] # random direction
 		d <- c(cos(d), sin(d)) # transform to x/y
 		bounds <- bound(x[[i-1]], d)
-		print(paste(c("x=", x[[i-1]], "d=", d, "b=", bounds)))
 		wasHit <- FALSE
 		while (!wasHit) {
 			l <- runif(1, bounds[1], bounds[2])[1] # random distance
 			xN <- x[[i-1]] + l * d
-			print(paste(c("l=", l, "xN=", xN)))
 			if (hit(xN)) {
 				x[[i]] <- xN
 				wasHit <- TRUE
-				print("HIT")
 			} else {
 				x[[i]] <- x[[i-1]]
 				# update bounds
@@ -116,37 +77,33 @@ har <- function(x0, niter, bound, hit) {
 				if (l < 0) {
 					bounds[1] <- l
 				}
-				print(paste(c("MISS; b=", bounds)))
 			}
 		}
 	}
 	x
 }
 
-# a bounding box of 2/3 around origin
+# a bounding box of sqrt(2/3) around origin
 boundBox <- function(x, d) {
+	lb <- c(-sqrt(2/3), -sqrt(2/3)) #FIXME: bounds correct for all n?
+	ub <- c(sqrt(2/3), sqrt(2/3))
 	c(
-		max(sapply(1:length(x), function(i) { min((-2/3 - x[i]) / d[i], (2/3 - x[i]) / d[i]) })),
-		min(sapply(1:length(x), function(i) { max((-2/3 - x[i]) / d[i], (2/3 - x[i]) / d[i]) }))
+		max(sapply(1:length(x), function(i) { min((lb[i] - x[i]) / d[i], (ub[i] - x[i]) / d[i]) })),
+		min(sapply(1:length(x), function(i) { max((lb[i] - x[i]) / d[i], (ub[i] - x[i]) / d[i]) }))
 	)
 }
-
-EPSILON <- 0.000001
 
 # hit calculation for (n-1)-dimensional simplex
 # basis: orthonormal basis for the (n-1)-dimensional simplex in n-dimensional space
 createHitSimplex <- function(basis) {
-#	correction <- basis %*% t(basis)
 	function(x) {
 		x <- basis %*% x # change of basis
-#		x <- correction %*% x # project onto plane
 		w <- 1/3 + x # translation (FIXME: correct for all n?)
 		allBounds <- min(sapply(w, function(wi) { wi >= 0 }))
-		sumBounds <- min(c(w[1] + w[2] <= 1, w[1] + w[3] <= 1, w[2] + w[3] <= 1))
+		sumBounds <- min(c(w[1] + w[2] <= 1, w[1] + w[3] <= 1, w[2] + w[3] <= 1)) # FIXME: generalize
 		allBounds && sumBounds
 	}
 }
-
 
 hit <- createHitSimplex(on)
 samples <- har(c(0,0), 1000, boundBox, hit)
