64396092e7f9af30c5b5ca469a213ca5cdd5ade6
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-12-12 12:10:45 +0100

    Add inverse transformation of simplex

--- hitandrun/R/transformSimplex.R
+++ hitandrun/R/transformSimplex.R
@@ -6,15 +6,31 @@ simplex.basis <- function(n) {
 
 # Generate a projection matrix that transforms an (n-1) dimensional vector in
 # homogeneous coordinate representation to an n-dimensional weight vector.
-simplex.createTransform <- function(n) {
+simplex.createTransform <- function(n, inverse=FALSE, keepHomogeneous=inverse) {
 	basis <- simplex.basis(n)
 	# add one extra element to vectors in each basis (homogeneous coordinate
 	# representation)
 	basis <- rbind(cbind(basis, rep(0, n)), c(rep(0, n - 1), 1))
+
 	# create translation matrix (using homogenous coordinates)
-	translation <- cbind(diag(n), rep(1/n, n))
-	# successively apply basis transformation and translation
-	translation %*% basis
+	dx <- if (inverse == FALSE) { 1/n } else { -1/n }
+	translation <- rbind(cbind(diag(n), rep(dx, n)), c(rep(0, n), 1))
+
+	# homogeneous coordinate elimination
+	nh <- if (inverse == FALSE) { n + 1 } else { n }
+	elimHom <- if (keepHomogeneous) {
+		diag(nh)
+	} else {
+		cbind(diag(nh - 1), rep(0, nh - 1))
+	}
+
+	if (inverse == FALSE) {
+		# successively apply basis transformation and translation
+		elimHom %*% translation %*% basis
+	} else {
+		# successively apply translation and basis transformation
+		elimHom %*% t(basis) %*% translation
+	}
 }
 
 # translate the n-dimensional constraints to the (n-1)-dimensional space
