1bf64f67c28ed212a0c7518b15e48600a82a85f4
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2011-12-15 14:50:07 +0100

    Add methods for starting point generation

--- hitandrun/R/bound.R
+++ hitandrun/R/bound.R
@@ -1,28 +1,30 @@
+homogeneousCoordinateConstraint <- function(n) {
+	list(constr=c(rep(0, n), 1), rhs=c(1), dir=c("="))
+}
+
+
 findExtremePoints <- function(constr, homogeneous=FALSE) {
 	n <- dim(constr$constr)[2]
 	if (homogeneous == TRUE) {
 		n <- n - 1
 		# add the constraint that the homogenous coordinate equals 1
-		constr$constr <- rbind(constr$constr, c(rep(0, n), 1))
-		constr$rhs <- c(constr$rhs, 1)
-		constr$dir <- c(constr$dir, "=")
+		constr <- mergeConstraints(constr, homogeneousCoordinateConstraint(n))
 	}
 
 	# because lp_solve assumes vars to be non-negative, split each dimension:
 	# x_i = y_{2i-1} - y_{2i}
 	# thus each dimension is coded as the difference of two variables, give them
 	obj <- function(i) {
-		obj <- rep(0, 2 * n)
+		obj <- rep(0, 2 * n + if (homogeneous) { 1 } else { 0 })
 		obj[2 * i - 1] <- 1
 		obj[2 * i] <- -1
 		obj
 	}
 	unsplit <- t(sapply(1:n, obj))
-	mat <- unsplit
 	if (homogeneous == TRUE) { # add row to preserve homogeneous coordinate
-		mat <- rbind(unsplit, c(rep(0, 2 * n - 1), 1))
+		unsplit <- rbind(unsplit, c(rep(0, 2 * n), 1))
 	}
-	a <- constr$constr %*% mat
+	a <- constr$constr %*% unsplit
 
 	# for each of the n dimensions, solve 2 LPs to find the min/max
 	findExtreme <- function(dir) {
@@ -31,19 +33,53 @@ findExtremePoints <- function(constr, homogeneous=FALSE) {
 		}
 	}
 	rawExtremes <- cbind(sapply(1:n, findExtreme("min")), sapply(1:n, findExtreme("max")))
-	unsplit %*% rawExtremes
+	t(unsplit %*% rawExtremes)
+}
+
+findVertices <- function(constr, homogeneous=FALSE) {
+	h <- if (homogeneous == TRUE) {
+		n <- dim(constr$constr)[2]
+		hom <- homogeneousCoordinateConstraint(n - 1)
+		makeH(constr$constr, constr$rhs, hom$constr, hom$rhs)
+	} else {
+		makeH(constr$constr, constr$rhs)
+	}
+
+	v <- q2d(scdd(d2q(h))$output)
+	# Check that the output are vertices, not other things that would indicate
+	# a bug
+	stopifnot(v[,1] == "0")
+	stopifnot(v[,2] == "1")
+
+	# Return the vertices only 
+	v[,-c(1,2)]
 }
 
 # generate seed point from constraints (TODO: implement multiple methods)
-createSeedPoint <- function(constr, homogeneous=FALSE) {
+createSeedPoint <- function(constr, homogeneous=FALSE, randomize=FALSE,
+		method="extremes") {
+	stopifnot(method %in% c("extremes", "vertices"))
+
 	n <- dim(constr$constr)[2]
 	if (homogeneous == TRUE) {
 		n <- n - 1
 	}
 
-	extreme <- findExtremePoints(constr, homogeneous)
-	# starting point (approximation of centroid)
-	p <- (1/(2*n)) * apply(extreme, 1, sum)
+	extreme <- if (method == "extremes") {
+		findExtremePoints(constr, homogeneous)
+	} else {
+		findVertices(constr, homogeneous)
+	}
+
+	# starting point 
+	m <- dim(extreme)[2]
+	p <- if (randomize == TRUE) { # random weighting
+		w <- as.vector(simplex.sample(m, 1)$samples)
+		apply(extreme, 2, function(row) { sum(w * row) })
+	} else { # mean: approximation of centroid
+		(1/m) * apply(extreme, 2, sum)
+	}
+
 	if (homogeneous == TRUE) {
 		p[n + 1] <- 1.0 # eliminate floating point imprecision
 	}
@@ -54,8 +90,11 @@ createSeedPoint <- function(constr, homogeneous=FALSE) {
 createBoundBox <- function(constr, homogeneous=FALSE) {
 	n <- dim(constr$constr)[2]
 	extreme <- findExtremePoints(constr, homogeneous)
+	if (homogeneous == TRUE) {
+		extreme <- extreme[ , 1:(n-1)]
+	}
 	# upper and lower bounds for each dimension in the (n-1) basis
-	lb <- apply(extreme, 1, min)
-	ub <- apply(extreme, 1, max)
-	list(lb=lb, ub=ub, extreme=extreme)
+	lb <- apply(extreme, 2, min)
+	ub <- apply(extreme, 2, max)
+	list(lb=lb, ub=ub)
 }
